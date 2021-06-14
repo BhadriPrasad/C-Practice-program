eb5ac168bfc088a9ee5e8146afc599fbba718250
BhadriPrasad <85891264+BhadriPrasad@users.noreply.github.com>
2021-06-14 21:20:04 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+class Program
+{
+  static void Main(string[] args)
+  {
+     char op,a;
+     double num1,num2;
+    while(true)
+    {
+           Console.WriteLine("Enter number 1:");
+      num1=Convert.ToDouble(Console.ReadLine());
+     Console.WriteLine("Enter number 2:");
+     num2=Convert.ToDouble(Console.ReadLine());
+     Console.WriteLine("Enter Symbol ( + - * / ):");
+     op=Console.ReadLine()[0];
+      switch(op)
+     {
+       case '+':
+       Console.WriteLine("The answer is {0}",num1+num2);
+       break;
+
+       case '-':
+       Console.WriteLine("The answer is {0}",num1-num2);
+       break;
+
+       case '*':
+       Console.WriteLine("The answer is {0}",num1*num2);
+       break;
+
+       case '/':
+       Console.WriteLine("The answer is {0}",num1/num2);
+       break;
+       default:
+       Console.WriteLine("invalid");
+       break;
+
+     }
+     Console.WriteLine("Do You want To Continue?(Y/N) ");
+     a=Convert.ToChar(Console.ReadLine());
+     if (a!='Y')
+     break;
+     
+     
+    
+
+
+
+
+      
+
+  }
+}
+}
